07531f01282ea7abf874c69899f84d2c9a1e713b
jonaheinke <jona-heinke@web.de>
2021-03-11 15:42:53 +0100

    fixed css loading, css split (portrait/landscape)

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ def get_year():
 	year = datetime.now().year
 	return ("2021-" if year > 2021 else "") + str(year)
 
+#theme_names = ["light", "dark"]
+
 class Root(object):
 	@cherrypy.expose
-	def index(self):
+	def index(self, t=0):
+		"""if not t:
+			t = 0"""
 		template = env.get_template("index.html")
-		return template.render(year = get_year())
+		return template.render(year = get_year()) #theme = theme_names[t], 
 
 cherrypy.quickstart(Root(), "/", "app.config")
