b7e4426459a988ea762ee264d0718cbf42d00105
jonaheinke <jona-heinke@web.de>
2021-04-30 23:45:42 +0200

    improved default behavior if no cookie is set -> use prefer-color-scheme

--- content/js/base.js
+++ content/js/base.js
@@ -1,14 +1,27 @@
 var body = null;
-var earths_shadow = null;
-var darkmode = {{darkmode['int']}};
+//var earths_shadow = null;
+var cookie = document.cookie;
+var darkmode = cookie[cookie.indexOf("=") + 1] != "0";
+
+function set_cookie() {
+	document.cookie = "darkmode=" + +darkmode + "; SameSite=Strict; path=/";
+}
+
+if(cookie.indexOf("darkmode=") == -1) {
+	darkmode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+	set_cookie();
+	if(darkmode) window.location.reload();
+}
 
 function toggle_theme() {
-	if(body != null) body.classList.toggle("dark");
-	darkmode = !darkmode;
-	document.cookie = "darkmode=" + (darkmode ? 1 : 0) + "; path=/";
+	if(body != null) {
+		body.classList.toggle("dark");
+		darkmode = !darkmode;
+		set_cookie();
+	}
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-	earths_shadow = document.querySelector(".slider::after");
+	//earths_shadow = document.querySelector(".slider::after");
 	body = document.querySelector("body");
 });
