2ca0e68449597b11948d075a6b484bb4b4d8fb5a
jonaheinke <jona-heinke@web.de>
2021-06-12 22:20:39 +0200

    NGRAM test and its implementation into search engine

--- search_engine.py
+++ search_engine.py
@@ -1,33 +1,64 @@
-import os
-from whoosh.fields import Schema, KEYWORD, TEXT, STORED
+import os, time, threading
+from whoosh.fields import Schema, KEYWORD, NGRAMWORDS, NUMERIC, TEXT
 from whoosh.index import create_in, open_dir
+from whoosh.writing import AsyncWriter
+from whoosh.qparser import QueryParser, MultifieldParser, FieldsPlugin, FieldAliasPlugin
+#from whoosh.analysis import StandardAnalyzer, StemmingAnalyzer, NgramFilter, NgramAnalyzer
+#from whoosh.query import *
 
-schema = Schema(id = STORED, title = TEXT, ingredients = KEYWORD, content = TEXT) #https://whoosh.readthedocs.io/en/latest/quickstart.html
+#https://whoosh.readthedocs.io/en/latest/quickstart.html
+schema = Schema(
+	id = NUMERIC(stored = True, unique = True, signed = False),
+	category = TEXT,
+	title = NGRAMWORDS(2, 20, True, 2.0),
+	ingredients = KEYWORD,
+	content = NGRAMWORDS(4, 20))
 
-class RecipeIndex():
-	ALWAYS_REBUILD = True
+#TODO: Synonyme https://whoosh.readthedocs.io/en/latest/api/lang/wordnet.html
+search_path = "search"
+ALWAYS_REBUILD = False
+min_search_length = 2
 
-	def __init__(self, path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "search")): #open or create index
-		if not self.ALWAYS_REBUILD and os.path.exists(path):
-			self.index = open_dir(path)
-		else:
-			self.index = create_in(path, schema)
+if not os.path.exists(search_path):
+	os.mkdir(search_path)
 
-	def rewrite_search_index(self):
-		writer = self.index.writer()
-		for i in range(3):
-			writer.add_document()
-		writer.commit()
-	
-	def add_document_to_index(self):
-		writer = self.index.writer()
-		writer.add_document()
-		writer.commit()
-	
-	def search(self, q):
-		with self.index.searcher() as searcher:
-			result = searcher.search(q)
-		return result
+def rebuild_index():
+	index = create_in(search_path, schema)
+	writer = index.writer()
+	writer.add_document(id = 0, title = "Test Words", content = "super nice")
+	writer.add_document(id = 1, title = "Apple Banana Cucumber")
+	writer.add_document(id = 2, title = "Deck Elevator Floor", category = "test")
+	writer.add_document(id = 3, title = "Pen Pineapple Apple Pen")
+	writer.add_document(id = 4, title = "wordsalad")
+	writer.add_document(id = 5, title = "injection")
+	writer.commit()
+	return index
 
 if __name__ == "__main__":
-    pass
+	rebuild_index()
+else:
+	if ALWAYS_REBUILD:
+		index = rebuild_index()
+	else:
+		index = open_dir(search_path)
+	
+	#TODO: Doesnt work. Only triggers once.
+	#timer = threading.Timer(86400, index.optimize)
+	#timer.daemon = True
+	#timer.start()
+
+	writer = AsyncWriter(index, 0.5)
+
+	parser = MultifieldParser(["content", "title"], schema, {"content": 1.0, "title": 2.0})
+	#parser.add_plugin(SingleQuotePlugin())
+	parser.remove_plugin_class(FieldsPlugin) #https://whoosh.readthedocs.io/en/latest/api/qparser.html#plug-ins
+
+	def search(q):
+		if len(q) < min_search_length:
+			return []
+		result = []
+		with index.searcher() as searcher:
+			search_result = searcher.search(parser.parse(q))
+			#result = list(map(lambda x: x.fields(), search_result))
+			result = [item.fields() for item in search_result]
+		return result
